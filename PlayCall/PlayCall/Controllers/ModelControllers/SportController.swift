cd9cb611675dc102dcf5a6f842acb5a586044bf3
Jarren Campos <jarrenmcampos@gmail.com>
2020-02-28 18:17:28 -0800

    Added View Controllers

---
+++ PlayCall/PlayCall/Controllers/ModelControllers/SportController.swift
@@ -0,0 +1,19 @@
+//
+//  SportController.swift
+//  PlayCall
+//
+//  Created by Jarren Campos on 2/26/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+import Foundation
+
+class SportController {
+    
+    var sportList = [
+        "Baseball",
+        "Soccer",
+        "Basketball",
+        "Football"
+    ]
+}
--- PlayCall/PlayCall/Controllers/ViewControllers/TeamSelectionViewController.swift
+++ PlayCall/PlayCall/Controllers/ViewControllers/TeamSelectionViewController.swift
@@ -9,14 +9,95 @@
 import UIKit
 
 class TeamSelectionViewController: UIViewController {
-
-    @IBOutlet weak var teamSelectionNextButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        teamSelectionNextButton.layer.cornerRadius = 25
+    
+    let teamController = TeamController()
+    
+    //changed to var from let to function
+    var selectedLevel = LevelSelectionViewController.selectedLevel ?? "High School"
+    var selectedSport = SportSelectionViewController.selectedSport ?? "Baseball"
+    
+    static var selectedTeam: String?
+    var teamType: [String] {
+        if selectedLevel == "High School"{
+            if selectedSport == "Football"{
+                return teamController.footballHighSchoolTeamList
+            }
+            else if selectedSport == "Baseball" {
+                return teamController.baseballHighSchoolTeamList
+            }
+            else if selectedSport == "Basketball" {
+                return teamController.basketballHighSchoolTeamList
+            }
+            else if selectedSport == "Soccer" {
+                return teamController.soccerHighSchoolTeamList
+            }
+            else{
+                return ["ERROR IN HS SELECT"]
+            }
+        }
+        if selectedLevel == "College"{
+            if selectedSport == "Football"{
+                return teamController.footballCollegeTeamList
+            }
+            else if selectedSport == "Baseball" {
+                return teamController.baseballCollegeTeamList
+            }
+            else if selectedSport == "Basketball" {
+                return teamController.basketballCollegeTeamList
+            }
+            else if selectedSport == "Soccer" {
+                return teamController.soccerCollegeTeamList
+            }
+            else{
+                return ["ERROR IN COLLEGE SELECT"]
+            }
+        }
+        if selectedLevel == "Professional"{
+            if selectedSport == "Football"{
+                return teamController.footballProTeamList
+            }
+            else if selectedSport == "Baseball" {
+                return teamController.baseballProTeamList
+            }
+            else if selectedSport == "Basketball" {
+                return teamController.basketballProTeamList
+            }
+            else if selectedSport == "Soccer" {
+                return teamController.soccerProTeamList
+            }
+            else{
+                return ["ERROR IN PRO SELECT"]
+            }
+        }
+        else {return ["ERROR IN LEVEL SELECT"]}
         
     }
     
-
+    @IBOutlet var teamPickerView: UIPickerView!
+    @IBOutlet weak var teamSelectionNextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        teamPickerView.dataSource = self
+        teamPickerView.delegate = self
+        teamSelectionNextButton.layer.cornerRadius = 25
+    }
+    
+    
+}
+extension TeamSelectionViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return teamType.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return teamType[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        SportSelectionViewController.selectedSport = teamType[row]
+    }
+    
 }
